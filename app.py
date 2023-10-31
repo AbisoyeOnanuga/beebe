f164ebdeba84114fe9cbfe407567b9c264ce30f4
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2023-10-31 10:31:59 -0400

    app update

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 # Import the necessary modules
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import os
+import torch
+import json
+from flask_cors import CORS, cross_origin
 
 # Create a Flask app object and specify the static_folder argument
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
+
+# Enable CORS for all domains and all routes
+cors = CORS(app, resources={r"/generate": {"origins": ["http://localhost:8000"]}})
 
 # Define a route for rendering the index.html file
 @app.route('/')
@@ -12,14 +18,16 @@ def index():
     return render_template('index.html')
 
 # Load the GPT-2 model and tokenizer from the local folder
-model_path = os.path.join(os.getcwd(), "gpt2")
+model_path = os.path.join(os.getcwd(), "output-gpt2")
+tokenizer_path = os.path.join(os.getcwd(), "gpt2")
 model = GPT2LMHeadModel.from_pretrained(model_path)
-tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
 
 # Define a function for generating responses using the model
 def generate_response(user_input):
     # Encode the user input and add the end-of-text token
-    input_ids = tokenizer.encode(user_input, return_tensors="pt", add_special_tokens=True)
+    input_ids = tokenizer.encode(user_input, return_tensors="pt")
+    input_ids = torch.cat([input_ids, torch.tensor([tokenizer.eos_token_id]).unsqueeze(0)], dim=-1)
     # Generate a response using the model
     output_ids = model.generate(input_ids, max_length=50, do_sample=True, top_p=0.9)
     # Decode the output and remove the end-of-text token
@@ -29,15 +37,26 @@ def generate_response(user_input):
 
 # Define a route for generating responses
 @app.route('/generate', methods=["POST"])
+# Use the cross_origin decorator to enable CORS for this route
+@cross_origin()
 def generate():
-    # Get the user input from the request body
-    user_input = request.json["user_input"]
-    # Generate a response using the model
-    model_output = generate_response(user_input)
-    # Return the output as a JSON object
-    return jsonify({"model_output": model_output})
+  # Get the user input from the request body
+  user_input = request.json["user_input"]
+  # Try to generate a response using the model
+  try:
+    chatbot_output = generate_response(user_input)
+    # Convert the output to a JSON string
+    chatbot_output = json.dumps({"model_output": chatbot_output})
+    # Return the output as a JSON string with the application/json content type
+    return chatbot_output, 200, {"Content-Type": "application/json"}
+  except Exception as e:
+    # Log the exception in the console
+    print(e)
+    # Return an error message as a JSON string with the application/json content type
+    return json.dumps({"model_output": "Sorry, something went wrong. Please try again later."}), 500, {"Content-Type": "application/json"}
 
 # Run the Flask app with debug mode and extra files to watch for changes
 app.config["DEBUG"] = True
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["EXTRA_FILES"] = ["static/index.html", "static/js/script.js", "static/css/style.css"]
+app.run(port=8000)
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,7 @@
-// Define the function for sending user input to the backend service and receiving model output as a response
+// Get the chatbox element by its id
+let chatbox = document.getElementById("chatbox");
+
+// Define the function for sending user input to the Flask app and receiving model output as a response
 async function sendMessage(e) {
   // Prevent default behaviour of send event
   e.preventDefault();
@@ -6,53 +9,73 @@ async function sendMessage(e) {
   let userInput = document.getElementById("input-field").value;
   // Clear the input field
   document.getElementById("input-field").value = "";
-  // Display the user input in the conversation div using innerHTML
-  chatbox.insertAdjacentHTML("beforeend", `<div class="user-message">${userInput}</div>`);  // Scroll to the bottom of the conversation div
-  conversation.scrollTop = conversation.scrollHeight;
   
-  // Send a POST request to the backend service with the user input as a JSON body
-  try {
-    let response = await fetch("http://localhost:5000/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({"user_input": userInput})
-    });
-    // Check the status of the response
-    if (response.ok) {
-      // Parse the response as a JSON object
-      let data = await response.json();
-      
-      // Get the model output from the JSON object
-      let chatbotOutput = data["model_output"];
+  // Display the user input in the chatbox element using innerHTML
+  chatbox.insertAdjacentHTML("beforeend", `<div class="user-message">${userInput}</div>`);  
+  // Scroll to the bottom of the chatbox element
+  chatbox.scrollTop = chatbox.scrollHeight;
+  
+  // Check if there is already a request in progress
+  if (isLoading) {
+    // If yes, do nothing and return
+    return;
+  }
+  // If no, set the isLoading variable to true
+  isLoading = true;
+  
+  // Send a POST request to the Flask app with the user input as a JSON body
+  // Use fetch instead of $.ajax
+  let response = await fetch("http://localhost:8000/generate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ "user_input": userInput }),
+  });
+  
+  // Set the isLoading variable back to false
+  isLoading = false;
+  
+  // Check the status of the response
+  if (response.ok) {
+    // Parse the response as a JSON object
+    let data = await response.json();
+    // Get the model output from the JSON object
+    let chatbotOutput = data["model_output"];
+    // Display the chatbot output in the chatbox element using innerHTML
+    chatbox.insertAdjacentHTML("beforeend", `<div class="chatbot-message">${chatbotOutput}</div>`);
+  } else {
+    // Throw an error if the response is not ok
+    throw new Error("Something went wrong");
+  }
+  
+  // Return false to prevent the default action
+  return false;
+}
 
-      // Return the chatbot output as a promise
-      return chatbotOutput;
-    } else {
-      // Throw an error if the response is not ok
-      throw new Error("Something went wrong");
-    }
-  } catch (error) {
-    // Log the error in the console
-    console.error(error);
+// Define a variable to store the number of messages in the chatbox
+let messageCount = 0;
+
+// Define a function that checks if the chatbox is full and restarts the conversation if so
+function checkChatFull() {
+  // Increment the message count by one
+  messageCount++;
+  // If the message count reaches 30, clear the chatbox and display a restart message
+  if (messageCount === 30) {
+    chatbox.innerHTML = "";
+    chatbox.insertAdjacentHTML("beforeend", `<div class="restart-message">Chat full, please restart the conversation.</div>`);
+    // Reset the message count to zero
+    messageCount = 0;
   }
 }
 
-function displayMessage(chatbotOutput) {
-  // Get the chatbox element by its id
-  let chatbox = document.getElementById("chatbox");
-  // Get the user input from the input field
-  let userInput = document.getElementById("input-field").value;
-  // Display the user input in the chatbox element using innerHTML
-  chatbox.insertAdjacentHTML("beforeend", `<div class="user-message">${userInput}</div>`);
-  // Display the chatbot output in the chatbox element using innerHTML
-  chatbox.insertAdjacentHTML("beforeend", `<div class="chatbot-message">${chatbotOutput}</div>`);
-}
-
+// Declare a global variable to store the request status
+var isLoading = false;
 
 // Add an event listener for the send button
-document.getElementById("send").addEventListener("click", sendMessage)
-{
-  displayMessage();
-}
+document.getElementById("send").addEventListener("click", function(e) {
+  // Call the sendMessage function with the event object
+  sendMessage(e);
+  // Call the checkChatFull function
+  checkChatFull();
+});
